67242c8d3b9c74f2c36807898b871c53f85a73ac
icsantos <isantos1206@gmail.com>
2019-05-10 05:01:22 -0500

    refactor: Wrap XMLHttpRequest in a Promise

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,23 +8,23 @@ Instructions:
 
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
+/* jshint esversion: 6 */
 
 (function(document) {
-  'use strict';
-
-  var home = null;
+  let home = null;
 
   /**
    * Helper function to show the search query.
    * @param {String} response - The unparsed JSON response from get.
+   * @returns {undefined}
    */
   function addSearchHeader(response) {
     try {
-      response = JSON.parse(response).query;  // you'll be moving this line out of here in the next quiz!
+      response = JSON.parse(response).query;  // You'll be moving this line out of here in the next quiz!
     } catch (e) {
-      // it's 'unknown', so leave it alone
+      // It's 'unknown', so leave it alone
     }
-    home.innerHTML = '<h2 class="page-title">query: ' + response + '</h2>';
+    home.innerHTML = `<h2 class="page-title">query: ${response}</h2>`;
   }
 
   /**
@@ -33,34 +33,38 @@ Instructions:
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    /*
-    This code needs to get wrapped in a Promise!
-     */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
-        // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+    return new Promise(function (resolve, reject) {
+      const req = new XMLHttpRequest();
+
+      req.open('GET', url);
+
+      req.onload = function() {
+        if (req.status === 200) {
+          resolve(req.response);
+        } else {
+          reject(Error(req.statusText));
+        }
+      };
+
+      req.onerror = function() {
+        reject(Error('Network Error'));
+      };
+
+      req.send();
+    });
   }
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Uncomment the next line you're ready to start chaining and testing!
-    You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
-    pass 'unknown' to addSearchHeader if it rejects.
-     */
-    // get('../data/earth-like-results.json')
+
+    get('../data/earth-like-results.json')
+      .then(function(response) {
+        addSearchHeader(response);
+      })
+      .catch(function(error) {
+        addSearchHeader('unknown');
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
   });
-})(document);
+}(document));
